6a40d8d6cafb2e75fb9d8d112b7bd65d1435349a
Kushaljain11 <kushaljain112006@gmail.com>
2025-01-21 15:11:01 +0530

    Add files via upload

---
+++ chapter2/problem 1.py
@@ -0,0 +1,16 @@
+def ls2():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    if a > b:
+        print("a is greater than b")
+    elif b > a:
+        print("b is greater than a")
+    else:
+        print("a is equal to b")
+
+ls2()
+ls2()
+ls2()
+
+
+
