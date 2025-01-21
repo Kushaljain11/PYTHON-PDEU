2a56493d71fb1d11256edf3cf8cd32bc2f178fcd
Kushaljain11 <kushaljain112006@gmail.com>
2025-01-21 15:56:12 +0530

    Add files via upload

---
+++ chapter2/problem 8.py
@@ -0,0 +1,12 @@
+def triangle():
+      a = int(input("Enter first angle: "))
+      b= int(input("Enter second angle: "))
+      c = int(input("Enter third angle: "))
+      if a+b+c==180:
+          print("triangle is valid")
+      else:
+          print("triangle is not valid")
+triangle()
+triangle()
+          
+    
---
+++ chapter2/problem 9.py
@@ -0,0 +1,6 @@
+def absolute():
+   
+    number = float(input("Enter a number: "))
+    print("The absolute value of the number is:", abs(number))
+absolute()
+absolute()
