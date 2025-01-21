b98c4c91e9134f2efa91c7d7fc77a12222c9101a
Kushaljain11 <kushaljain112006@gmail.com>
2025-01-21 16:03:10 +0530

    Add files via upload

---
+++ chapter2/problem 10.py
@@ -0,0 +1,12 @@
+def rectangle():
+    l=12
+    b=24
+    area=l * b
+    perimeter=2 * (l+b)
+    if area>perimeter:
+        print("the area is greater")
+    else:
+        print("the perimeter is greater")
+rectangle()
+
+        
