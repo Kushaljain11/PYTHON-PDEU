ffce7dc937ddb468ee17789a189a6f23f019290f
Kushaljain11 <kushaljain112006@gmail.com>
2025-02-25 01:42:55 +0530

    Add files via upload

---
+++ loops/pg1.py
@@ -0,0 +1,10 @@
+# Uppercase letters
+print("Uppercase:")
+for i in range(65, 91):
+    print(chr(i), end=' ')
+print()
+
+# Lowercase letters
+print("Lowercase:")
+for i in range(97, 123):
+    print(chr(i), end=' ')
---
+++ loops/pg3.py
@@ -0,0 +1,11 @@
+s = input("Enter a string: ")
+alphabets = 0
+digits = 0
+
+for char in s:
+    if char.isalpha():
+        alphabets += 1
+    elif char.isdigit():
+        digits += 1
+
+print(f"Alphabets: {alphabets}, Digits: {digits}")
