3b4f8555e8fc3f8bdd0339919225a89fa71c73fa
Kushaljain11 <kushaljain112006@gmail.com>
2025-02-25 14:42:39 +0530

    Delete set/# List of words.py

--- set/# List of words.py
+++
@@ -1,8 +0,0 @@
-# List of words
-words = ["apple", "banana", "cherry", "date", "elderberry"]
-
-# Convert words to uppercase and store them in a set
-uppercase_words = set(word.upper() for word in words)
-
-# Display the set of uppercase words
-print(uppercase_words)
