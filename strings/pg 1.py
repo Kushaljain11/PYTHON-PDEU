23a1f6d045fe8f0263703d4877a18bd3b66a4fd6
Kushaljain11 <kushaljain112006@gmail.com>
2025-02-27 01:04:40 +0530

    Update pg 1.py

--- strings/pg 1.py
+++ strings/pg 1.py
@@ -1,5 +1,5 @@
-string = input("Enter a string: ")  # Accept input from user
-vowels = "aeiouAEIOU"  # Define vowels
-count = sum(1 for char in string if char in vowels)  # Count vowels
+string = input("Enter a string: ")  
+vowels = "aeiouAEIOU"  
+count = sum(1 for char in string if char in vowels)  
 
 print("Number of vowels:", count)
