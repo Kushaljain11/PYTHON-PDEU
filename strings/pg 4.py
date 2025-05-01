df2055c153931d49ee0a5aefac484a585ca859f2
Kushal Pratik Jain <kushaljain112006@gmail.com>
2025-05-01 14:05:13 +0530

    Update pg 4.py

--- strings/pg 4.py
+++ strings/pg 4.py
@@ -1,17 +1,6 @@
 # ''' 4)	Write a function that removes one string from another string, if present.
 #  E.g. Onestring = “abcdef”, removestring = “cd”. The finalstring should contain “abef”.'''
-def remove_substring(s, sub):
-    result = ""
-    i = 0
-    while i < len(s):
-        if s[i:i+len(sub)] == sub:
-            i += len(sub)
-        else:
-            result += s[i]
-            i += 1
-    return result
+st1 = input("Enter string: ")
+remSt = input("Remove string: ")
 
-s = input("Enter the main string: ")
-sub = input("Enter the string to remove: ")
-
-print("Final string:", remove_substring(s, sub))
+print(st1.replace(remSt, ""))
