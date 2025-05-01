56efdc7bf45217089e43be56cdeca2c130939f80
Kushal Pratik Jain <kushaljain112006@gmail.com>
2025-05-01 13:58:27 +0530

    Update pg 3.py

--- strings/pg 3.py
+++ strings/pg 3.py
@@ -1,15 +1,20 @@
 # 3)	Accept two strings. Check whether one string is there in another string.
-def is_substring(s1, s2):
-    for i in range(len(s1) - len(s2) + 1):
-        if s1[i:i+len(s2)] == s2:
-            return True
+def isSame(st1, st2):
+    if len(st1) > len(st2):
+        for i in range(0, len(st1)):
+            if st2 == st1[:i]:
+                return True
+    elif len(st2) > len(st1):
+        for i in range(0, len(st2)):
+            if st1 == st2[:i]:
+                return True
+    elif st2 == st1:
+        return True
     return False
+st1 = input("String 1: ")
+st2 = input("String 2: ")
 
-s1 = input("Enter the first string: ")
-s2 = input("Enter the second string: ")
-
-if is_substring(s1, s2):
-    print("Yes, the second string is in the first string.")
-else:
-    print("No, the second string is not in the first string.")
-
+if isSame(st1,st2):
+    print("String is present in another")
+else: 
+    print("String is not present in another")
