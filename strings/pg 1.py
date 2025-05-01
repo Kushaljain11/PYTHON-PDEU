48bc3ce02f9d4c3c009a3cd3a6b9b1d4fbf7a127
Kushal Pratik Jain <kushaljain112006@gmail.com>
2025-05-01 12:11:59 +0530

    Update pg 1.py

--- strings/pg 1.py
+++ strings/pg 1.py
@@ -1,3 +1,4 @@
+# 1)	Count how many vowels are there in a string. Accept the string from the user.
 string = input("Enter a string: ")  
 vowels = "aeiouAEIOU"  
 count = sum(1 for char in string if char in vowels)  
