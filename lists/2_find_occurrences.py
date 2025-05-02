9ebacb984c8661b47590f1da5c5d1cf448347d26
Kushal Pratik Jain <kushaljain112006@gmail.com>
2025-05-02 08:42:13 +0530

    Update 2_find_occurrences.py

--- lists/2_find_occurrences.py
+++ lists/2_find_occurrences.py
@@ -1,3 +1,4 @@
+# 2.	Generate 20 random integers and store them in a list. Accept a number from the user and print position of all occurrences of that number in the list.
 import random
 
 nums = [random.randint(1, 50) for _ in range(20)]
