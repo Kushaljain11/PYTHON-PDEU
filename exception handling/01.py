f3f096bfbbf1ca60cbd8801926de662be1618047
Kushal Pratik Jain <kushaljain112006@gmail.com>
2025-05-02 09:37:44 +0530

    Update 01.py

--- exception handling/01.py
+++ exception handling/01.py
@@ -1,3 +1,7 @@
+# o	Write a program that receives an integer an input. If a string is entered instead of an integer, 
+# then report an error and give another chance to user to enter an integer. Continue this process till correct input is supplied.
+
+
 while True:
     try:
         user_input = int(input("Please enter an integer: "))
