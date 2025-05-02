30a7638fc6ebcf62a604077eb28f738f4b8a48c5
Kushal Pratik Jain <kushaljain112006@gmail.com>
2025-05-02 09:07:59 +0530

    Update program5.py

--- dictonary/program5.py
+++ dictonary/program5.py
@@ -1,4 +1,5 @@
-# 5. Create two dictionaries – one containing grocery items and their prices and another containing grocery items and quantity purchased. By using the values from these two dictionaries compute the total bill.
+# 5. Create two dictionaries – one containing grocery items and their prices and another containing grocery items and quantity purchased.
+# By using the values from these two dictionaries compute the total bill.
 prices = {'milk': 40, 'bread': 30, 'eggs': 10}
 quantities = {'milk': 2, 'bread': 1, 'eggs': 12}
 total_bill = 0
