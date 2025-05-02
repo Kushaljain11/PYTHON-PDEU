145484527787db3c0a47473149f0466f291392cc
Kushal Pratik Jain <kushaljain112006@gmail.com>
2025-05-02 09:10:56 +0530

    Update pg2.py

--- set/pg2.py
+++ set/pg2.py
@@ -1,3 +1,5 @@
+# 2.	Write a program to create a set containing 10 random numbers in the range 15 to 45. 
+# Count how many of these numbers are less than 30. Delete all numbers that are greater than 35.
 import random
 numbers = {random.randint(15, 45) for _ in range(10)}
 print(numbers)
